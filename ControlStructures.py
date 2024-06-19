b0420d96d93520fa963b3e5d7c0475fe951826b0
Rabia Asif <rabiaasif.2002@gmail.com>
2024-06-19 17:48:50 +0500

    first commit

---
+++ ControlStructures.py
@@ -0,0 +1,26 @@
+# Control Structures in Python
+# If-Else
+x = 10
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
+
+# For Loop
+for i in ['mosh', 'john', 'sarah']:
+    print(i)
+
+
+# While Loop
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+print("done")
+
+#Nested loop
+for x in range(4):
+    for y in range(3):
+        print(f'({x}, {y})')
+
+
